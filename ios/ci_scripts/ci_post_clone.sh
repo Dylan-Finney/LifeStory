50385db81feda14d9d6427e82057b4b7a0aec8b6
Dylan Finney <54768129+Dylan-Finney@users.noreply.github.com>
2025-06-16 21:35:49 +0100

    Fix for xCode cloud (Attempt 3)

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,16 +1,10 @@
-#!/bin/zsh
+#!/bin/sh
 
-echo "===== Installling CocoaPods ====="
-export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+# Install Node, CocoaPods, and yarn using Homebrew.
+brew install node
 brew install cocoapods
-echo "===== Installing Node.js ====="
-brew install node@21
-echo "===== Installing yarn ====="
 brew install yarn
 
 # Install dependencies
-echo "===== Running yarn install ====="
-yarn install
-echo "===== Running pod install ====="
-cd ios
+yarn
 pod install
